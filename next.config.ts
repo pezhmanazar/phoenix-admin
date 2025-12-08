956674ff5b046452e523fea3554a8e1ff7b808ff
pezhmanazar <dr.pezhman.azar@gmail.com>
2025-12-08 18:35:29 +0330

    Disable ESLint during production build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ⛔️ ESLint ارور نده موقع build پروداکشن
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
